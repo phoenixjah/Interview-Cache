00040fa3a8006719473ed183ede6ce89de27a519
phoenixjah <phoenixjah@gmail.com>
2012-07-10 16:20:25 +0800

    project init

---
+++ InterviewCache/InterviewCache/PeopleViewController.h
@@ -0,0 +1,13 @@
+//
+//  PeopleViewController.h
+//  InterviewCache
+//
+//  Created by Phx Lin. on 7/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PeopleViewController : UITableViewController
+
+@end
---
+++ InterviewCache/ProjectViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProjectViewController.h
+//  InterviewCache
+//
+//  Created by Phx Lin. on 7/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProjectViewController : UITableViewController
+
+@end
